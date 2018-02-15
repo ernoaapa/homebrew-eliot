babcbdc870bb7a428a00a1a229a194a16275dc65
goreleaserbot <goreleaser@carlosbecker.com>
2018-02-15 06:50:49 +0200

    eliot version v0.2.7

--- eliot.rb
+++ eliot.rb
@@ -1,9 +1,9 @@
 class Eliot < Formula
   desc "Container management system for IoT devices"
   homepage "http://eliot.run"
-  url "https://github.com/ernoaapa/eliot/releases/download/v0.2.6/eli_0.2.6_darwin_amd64.tar.gz"
-  version "0.2.6"
-  sha256 "98974bccfe6d48e17516cb0cbfa9171fba3bba42dce31e4a96bf96290d2bf023"
+  url "https://github.com/ernoaapa/eliot/releases/download/v0.2.7/eli_0.2.7_darwin_amd64.tar.gz"
+  version "0.2.7"
+  sha256 "504fa630212dcd52ac0ab961c19eb669b68cbf7fdb78c168a8590e72bf2ae908"
 
   def install
     bin.install "eli"
