b2c37c4f527d6212d12473f322da3782fb67a649
Erno Aapa <ernoaapa@users.noreply.github.com>
2018-03-25 13:23:03 +0300

    Fix invalid class name in eli.rb

--- eli.rb
+++ eli.rb
@@ -1,4 +1,4 @@
-class Eliot < Formula
+class Eli < Formula
   desc "Container management system for IoT devices"
   homepage "http://eliot.run"
   url "https://github.com/ernoaapa/eliot/releases/download/v0.2.8/eli_0.2.8_darwin_amd64.tar.gz"
